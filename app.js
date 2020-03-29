b108acad8d3ffc4510366c67d4ffc796b66f221a
Matthew <mattg1827@gmail.com>
2020-03-29 14:53:07 -0700

    Added routes, DB connection

--- app.js
+++ app.js
@@ -4,15 +4,43 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connected to database
+mongoose.connect(config.database);
+
+// On connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database' + config.database);
+});
+// Error connection
+mongoose.connection.on('error', (err) => {
+    console.log('Database error' + err);
+});
 
 const app = express();
+const users = require('./routes/users');
 
+// PORT number
 const port = 3000;
 
+// CORS Middleware
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index route
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
-})
+});
 
+// Start server
 app.listen(port, () => {
-    console.log('Server started on port: ' + port)
+    console.log('Server started on port: ' + port);
 });
